638a3dae7ed368ab2a170c4b416671221a970007
philipreese <pbreese42@gmail.com>
2025-05-22 16:55:38 -0400

    add login command

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { readConfig, setUser } from "./config";
+import { type CommandsRegistry, registerCommand, runCommand } from "./commands/commands.js";
+import { readConfig } from "./config";
+import { handlerLogin } from "./commands/users.js";
 
 function main() {
-    setUser("Philip");
+    const commandsRegistry: CommandsRegistry = {};
+    if (process.argv.length < 3) {
+        console.log("usage: cli <command> [args...]");
+        process.exit(1);
+    }
+    const commandName = process.argv[2];
+    const args = process.argv.slice(3);
+    registerCommand(commandsRegistry, commandName, handlerLogin);
+
+    try {
+        runCommand(commandsRegistry, commandName, ...args);
+    } catch (err) {
+        console.log(`Error running command ${commandName}: ${(err as Error).message}`);
+        process.exit(1);
+    }
     console.log(readConfig());
 }
 
